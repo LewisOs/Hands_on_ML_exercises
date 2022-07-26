0cf2f8d1292af3aae87e9e0f03bee1bd30eb0e3b
Lewis O'Sullivan <Lewis.os@hotmail.com>
2022-07-26 14:49:28 +0100

    starting exe 2

---
+++ Chapter 3/Ch3_Ex2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 26 14:46:59 2022
+
+Write a function that can shift an MNIST image in any direction (left, right, 
+up, down), but 1 pixel. Then, for each image in the training set, create four 
+shifted copies (one per direction) and add them to the training set. Finally, 
+train your best model on this expanded training set and measure its accuracy 
+on the test set. This technique of artificially growing or expanding the 
+training set is called data augmentation or training set expansion.
+
+
+@author: lewis
+"""
+
